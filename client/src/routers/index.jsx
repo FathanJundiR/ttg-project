e08e356a369c33dd5a5925eb585bb8eef6d4c775
RinasCode <rinasismita@gmail.com>
2024-08-14 17:43:24 +0700

    loader dan form register

--- client/src/routers/index.jsx
+++ client/src/routers/index.jsx
@@ -9,9 +9,48 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <LoginPage />,
+    loader: () => {
+      if (localStorage.getItem("access_token")) {
+        Toastify({
+          text: "You're already logged in",
+          duration: 3000,
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "left",
+          stopOnFocus: true,
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+          onClick: function () {},
+        }).showToast();
+        return redirect("/"); // Redirect to home if already logged in
+      }
+      return null;
+    },
   },
   {
     element: <BaseLayout/>,
+    loader: () => {
+      if (!localStorage.getItem("access_token")) {
+        Toastify({
+          text: "Please login first",
+          duration: 3000,
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "left",
+          stopOnFocus: true,
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+          onClick: function () {},
+        }).showToast();
+        return redirect("/login"); // Redirect to login if not authenticated
+      }
+      return null;
+    },
+	
     children:[
         {
             path: '/',
